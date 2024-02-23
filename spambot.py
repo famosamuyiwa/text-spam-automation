d77e3a15c2109115d7b07680a1712018d4008734
muyiwa <nenling19@gmail.com>
2024-02-23 23:27:56 +0100

    wait time update

--- spambot.py
+++ spambot.py
@@ -1,6 +1,11 @@
 import pyautogui, time
-time.sleep(9)
+time.sleep(20)
+
+
 f = open("beescript", 'r')
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
+    time.sleep(5)
+
+
